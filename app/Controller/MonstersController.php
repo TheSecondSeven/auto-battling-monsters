6cf70a209d86887643acf50d8e83dd4179e2493b
Michael Stratford <michael@jloop.com>
2019-08-17 12:15:32 -0700

    debugg

--- app/Controller/MonstersController.php
+++ app/Controller/MonstersController.php
@@ -671,10 +671,11 @@ class MonstersController extends AppController {
 				]
 			]
 		]);
-		
-		foreach($monsters as &$monster) {
+		$response = [];
+		foreach($monsters as $monster) {
 			$monster['Monster']['resting_until'] = strtotime($monster['Monster']['resting_until']);
 			$monster['Monster']['in_gauntlet_run_until'] = strtotime($monster['Monster']['in_gauntlet_run_until']);
+			$response[] = $monster;
 		}
 		
 		$this->set(compact('response'));
