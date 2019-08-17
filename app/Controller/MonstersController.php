4b6face9a65fa1d18a2b9792cad091f2aa1bf7ad
Michael Stratford <michael@jloop.com>
2019-08-17 12:31:50 -0700

    json parse error

--- app/Controller/MonstersController.php
+++ app/Controller/MonstersController.php
@@ -671,11 +671,11 @@ class MonstersController extends AppController {
 				]
 			]
 		]);
-		$response = [];
+		$response['Monsters'] = [];
 		foreach($monsters as $monster) {
 			$monster['Monster']['resting_until'] = strtotime($monster['Monster']['resting_until']);
 			$monster['Monster']['in_gauntlet_run_until'] = strtotime($monster['Monster']['in_gauntlet_run_until']);
-			$response[] = $monster;
+			$response['Monsters'][] = $monster;
 		}
 		
 		$this->set(compact('response'));
