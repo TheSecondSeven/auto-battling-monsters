3f2755ea12d7223433b9b0e44f990e3e206e8250
Michael Stratford <michael@jloop.com>
2024-05-30 17:04:42 -0700

    Update StartDreamingCommand.php

--- src/Command/StartDreamingCommand.php
+++ src/Command/StartDreamingCommand.php
@@ -1,21 +1,14 @@
 <?php
 namespace App\Command;
 
+use Cake\Command\Command;
 use Cake\Console\Arguments;
-use Cake\Console\Command;
 use Cake\Console\ConsoleIo;
-use Cake\Console\ConsoleOptionParser;
-use Cake\ORM\Query;
-use Cake\ORM\Query\SelectQuery;
-use Cake\I18n\DateTime;
 
 class StartDreamingCommand extends Command {
 
-    protected function buildOptionParser(ConsoleOptionParser $parser): ConsoleOptionParser {
-        return $parser;
-    }
-
     public function execute(Arguments $args, ConsoleIo $io) {
+        $io->out('Hello world.');
         $this->loadModel('Users');
         $users = $this->fetchTable('Users')
             ->find()
@@ -28,6 +21,7 @@ class StartDreamingCommand extends Command {
 		    $user->dreaming_since = new DateTime();
             $this->Users->save($user);
         }
+        return true;
     }
 }
 ?>
